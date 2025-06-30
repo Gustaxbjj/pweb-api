6eefa2c05328fe906ce7dbd503bbc3c98ae7d223
Gustaxbjj <153648794+Gustaxbjj@users.noreply.github.com>
2025-06-30 11:29:36 -0300

    organizar

---
+++ models/Canal.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+  return sequelize.define('Canal', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nome: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    data_criacao: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    genero_tema: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+  }, {
+    tableName: 'canais',
+    timestamps: false,
+  });
+};
